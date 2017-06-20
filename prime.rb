89cb486bd84f55a37b9502189c9e9ebdce2b8434
Snigdha Sur <sur.snigdha@gmail.com>
2017-06-20 20:29:44 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+
+def prime?(n)
+
+  array = (2..n-1.to_i).to_a
+  array2 = []
+  array.each do |i|
+    array2.push n % i
+  end
+  if n <= 1
+    return false
+  elsif n == 2 or n == 3
+    return true
+  else
+    for i in array2
+      if i == 0
+        return false
+      end
+    end
+    return true
+  end
+
+end
